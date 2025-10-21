4eb9b7dda0c4ef76747466e2641fe203840ffdfb
muskanL-ux <lathermuskan30@gmail.com>
2025-10-21 13:38:26 +1300

    started project work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+function NumberList(props){
+  const numbers = props.numbers;
+  const listItems = numbers.map((numbers) =>
+    <li> {number}</li>
+); 
+return(
+  <u1> {listItems}</u1>
+)
+
+
+}
+const num = [1, 2, 47];
+constnames=["john", "Sara", "Smith"]
 function App() {
+  
+  const [val, setVal] = useState(0);
+  const [name, setName] = useState("")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NumberList numbers = {nums}/>
+      <NumberList numbers = {names}/>
+      <h1> Current Value {val} </h1>
+      <button onClick={()=>setVal(val+1)}>Increment Value </button>
+      <button onClick={()=>setVal(val-1)}>Decrement Value </button>
+      
+      {val>=50000&& <h3> Value is greater than or equal to 50000</h3>}
+      <input type = "text"
+       onChange={(e) => setName(e.target.value)} />
+
+      <h3>Welcome {name} </h3>
+
     </div>
   );
+  
 }
 
 export default App;
